00f31a2244a88765827608c764ceea74b2a08ca8
John Doe <taranosov92@gmail.com>
2019-04-15 17:20:26 +0300

    react init lessons 23

--- lessons_v1/src/Car/Car.js
+++ lessons_v1/src/Car/Car.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import "./Car.css"
 
 
 
-const Car = (props) => (
-    <div style={
-      {
-        border: "1px solid #3ccc",
-        maxWidth: "200px",
-        margin: "15px auto",
-        padding: "10px"
-      }
-    }>
-      <p>Car name: <b>{props.name}</b></p>
-      <p>Car year: <b>{props.year}</b></p>
-      <input type="text" onChange={props.changeName} value={props.name}/>
-      <button onClick={props.deleteItem}>Delete this item</button>
+const Car = (props) => {
 
-    </div>
-);
+
+  const className = ['input'];
+
+  if(props.name !== ''){
+    className.push('valid')
+  } else {
+    className.push('error')
+  }
+
+
+  return (
+      <div style={
+        {
+          border: "1px solid #3ccc",
+          maxWidth: "200px",
+          margin: "15px auto",
+          padding: "10px"
+        }
+      }>
+        <p>Car name: <b>{props.name}</b></p>
+        <p>Car year: <b>{props.year}</b></p>
+        <input type="text" onChange={props.changeName} value={props.name} className={className.join(' ')}/>
+        <button onClick={props.deleteItem}>Delete this item</button>
+
+      </div>
+  )
+};
 
 
 
